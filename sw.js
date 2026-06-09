1ef51f6261e6b8054c8d4bde7a53a09788eb0a56
TFK <ops@thefeelkorea.com>
2026-06-09 13:18:09 +0900

    mobile v2.47.51 2026-06-09T04:18

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// v2.47.50 SW — .json/.enc/.html은 network-first, 자산은 cache-first
-const CACHE = 'thefeel-mobile-v2.47.50';
+// v2.47.51 SW — .json/.enc/.html은 network-first, 자산은 cache-first
+const CACHE = 'thefeel-mobile-v2.47.51';
 const ASSETS = [
   './',
   './index.html',
